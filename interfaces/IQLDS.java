d691a6cacb2953efc4e00e142bffdc371b3353fd
Hai tran do <10a3nguyenanhhai@gmail.com>
2020-04-08 08:28:21 +0700

    add QLDS class

--- interfaces/IQLDS.java
+++ interfaces/IQLDS.java
@@ -3,7 +3,10 @@ package interfaces;
 import util.CaNhan;
 
 public interface IQLDS {
+  // them ca nhan
   public abstract int them(CaNhan cn);
+  // xoa xa nhan
   public abstract int xoa(String ten);
+  // in ds ca nhan
   public abstract void inDS();
 }
